bc98d3e4c29b0cd4cc1d30935666e08fa8200533
Martin Briday <m.briday@ipnl.in2p3.fr>
2020-08-31 19:05:06 +0200

    Working on prospector code.

--- pysedkcorr/prospector/prospector.py
+++ pysedkcorr/prospector/prospector.py
@@ -4,6 +4,7 @@ import pandas
 import time
 import sys
 import os
+import warnings
 
 from . import io
 
@@ -64,20 +65,134 @@ class Prospector():
                               "unc":np.array(self.spec_in["flux.err"]) if "flux.err" in self.spec_in.keys() else None})
         self._obs = fix_obs(self._obs)
         
-    def build_model(self, model=None, templates=None):
+    def build_model(self, model=None, templates=None, verbose=False):
         """
+        Build the model parameters to fit on measurements.
         
+        Parameters
+        ----------
+        templates : [string or list(string) or None]
+            Prospector prepackaged parameter set(s) to load.
+            Can be one template, or a list.
+            Default is None.
+        
+        Options
+        -------
+        model :  [prospect.models.sedmodel.SedModel or dict]
+            Can load an already existing prospector compatible 'model' SedModel.
+            Can update a SedModel compatible 'model' dictionary before to create the SedModel on it.
+            Default is None.
+        
+        verbose : [bool]
+            If True, print information.
+            Default is False.
+        
+        
+        Returns
+        -------
+        Void
         """
-        if model is not None:
+        from prospect.models.sedmodel import SedModel
+        from prospect.models.templates import TemplateLibrary
+        from prospect.models import priors
+        
+        if type(model) is SedModel:
             self._model = model
             return
         
-        from prospect.models.sedmodel import SedModel
-        from prospect.models.templates import TemplateLibrary
+        if verbose:
+            self.describe_templates(templates=templates)
         
-        _model = {}
+        _model = {} if model is None else model
+        for _t in np.atleast_1d(templates):
+            if _t in TemplateLibrary._descriptions.keys():
+                _model.update(TemplateLibrary[_t])
+        for _p, _pv in _model.items():
+            try:
+                if _pv["prior"].__module__ != priors.__name__:
+                    _pv["prior"] = self._build_prior_(_pv["prior"])
+            except(AttributeError):
+                pass
+        
+        if verbose:
+            print("# ================= #\n#   Current model   #\n# ================= #\n")
+            print(_model)
         
         self._model = SedModel(_model)
+    
+    def modify_model(self, changes=None, removing=None, verbose=False):
+        """
+        Apply changes on the loaded model.
+        
+        Parameters
+        ----------
+        params : [dict or None]
+            Dictionary containing the desired changes for the desired parameters.
+            As an example, params={"zred"={"init"=2.}}) will change the initial value of the 'zred parameter, without changing anything else.
+            Default is None.
+        
+        removing : [string or list(string) or None]
+            Fill this input with every parameters you want to remove from the model.
+            Default is None.
+        
+        Options
+        -------
+        verbose : [bool]
+            If True, print information.
+            Default is False.
+        
+        
+        Returns
+        -------
+        Void
+        """
+        _model = self.model.config_dict
+        if verbose:
+            print("# ================== #\n#   Previous model   #\n# ================== #\n")
+            print(_model)
+        
+        #Changes
+        if changes is not None:
+            _changes = changes.copy()
+            for _p, _pv in _changes.items():
+                for _k, _kv in _pv.items():
+                    if _k is not in _model[_p].keys():
+                        warnings.warn("'{}' is not included in '{}' model parameters.".format(_k, _p))
+                        _pv.pop(_k)
+                    if _k == "prior":
+                        _pv[_k] = self._build_prior_(_kv)
+                _model[_p].update(_c)
+        
+        #Removing
+        if removing is not None:
+            for _t in np.atleast_1d(removing):
+                if _t in _model.keys():
+                    _model.pop(_t)
+        
+        if verbose:
+            print("\n\n\n# ============= #\n#   New model   #\n# ============= #\n")
+            print(_model)
+        
+        self._model = SedModel(_model)
+    
+    @staticmethod
+    def _build_prior_(prior):
+        """
+        Build and return a prospector SedModel compatible prior.
+        
+        Parameters
+        ----------
+        prior : [dict]
+            Dictionary containing every required parameters to build a prior object.
+            
+            
+        Returns
+        -------
+        prospect.models.priors.[chosen prior]
+        """
+        from prospect.models import priors
+        _name = prior.pop("name")
+        return eval("priors.{}".format(_name))(**prior)
     
     @staticmethod
     def describe_templates(templates=None):
@@ -102,17 +217,16 @@ class Prospector():
         print("# ======================= #\n#   Availbale templates   #\n# ======================= #\n")
         TemplateLibrary.show_contents()
         
+        if templates in ["*", "all"]:
+            templates = list(TemplateLibrary._descriptions.keys())
         if templates is not None and not np.all([_t not in TemplateLibrary._descriptions.keys() for _t in np.atleast_1d(templates)]):
-            if templates in ["*", "all"]:
-                templates = TemplateLibrary._descriptions.keys()
-            print("# ========================= #\n#   Detailed descriptions   #\n# ========================= #\n")
+            print("\n\n\n# ========================= #\n#   Detailed descriptions   #\n# ========================= #\n")
             for _t in np.atleast_1d(templates):
-                if _t not in TemplateLibrary._descriptions.keys():
-                    continue
-                dash_string = "".join(["-"]*(len(_t)+6))
-                print("+{}+\n|   {}   |\n+{}+".format(dash_string, _t, dash_string))
-                TemplateLibrary.describe(_t)
-                print("\n")
+                if _t in TemplateLibrary._descriptions.keys():
+                    dash_string = "".join(["-"]*(len(_t)+6))
+                    print("+{}+\n|   {}   |\n+{}+".format(dash_string, _t, dash_string))
+                    TemplateLibrary.describe(_t)
+                    print("\n")
     
     def build_sps(self, zcontinuous=1, sps=None):
         """
